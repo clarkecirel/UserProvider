998f536cf1b959546253d9176e4dec3d85ffb261
Clarke Cirel <Clarke.cirel@gmail.com>
2019-12-12 19:36:07 -0700

    Final

--- src/components/User.js
+++ src/components/User.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { Card, } from "semantic-ui-react";
 import { UserConsumer, } from "../providers/UserProvider";
+import { UserContext, } from "../providers/UserProvider";
 
-const User = () => (
-  <UserConsumer>
-    { value => (
-  <Card>
-    <Card.Content>
-      <Card.Header>{ value.username }</Card.Header>
-      <Card.Meta>
-        Date Joined: { value.dateJoined }
-      </Card.Meta>
-    </Card.Content>
-    <Card.Content>
-      <p>Membership Level: { value.membershipLevel }</p>
-    </Card.Content>
-  </Card>
-  )}
-  </UserConsumer>
-)
+const User = () => {
+  const user = useContext(UserContext);
+
+  return (
+    <Card>
+      <Card.Content>
+        <Card.Header>{ user.username }</Card.Header>
+        <Card.Meta>
+          Date Joined: { user.dateJoined }
+        </Card.Meta>
+      </Card.Content>
+      <Card.Content>
+        <p>Membership Level: { user.membershipLevel }</p>
+      </Card.Content>
+    </Card>
+  );
+};
 
 export default User;
